563b93948444e89c51669474435d38e0c4198f3e
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2023-06-24 15:00:55 -0500

    push to git

---
+++ README.md
@@ -0,0 +1,3 @@
+# omegle-messenger
+A tool that makes interacting with Omegle web chat easy. It allows for sending of messages through code, and processing of user and stranger's messages.
+
---
+++ src/messenger.js
@@ -0,0 +1,91 @@
+const Event = require('simply-pub-sub').Event;
+
+function Messenger(opts) {
+  const body = document.querySelector('body');
+  const bodyObserver = new MutationObserver(bodyCallback);
+  const chatObserver = new MutationObserver(chatCallback);
+  const msgEvent = Event();
+  let chatBtn;
+  let textarea;
+  let sendBtn;
+  let endBtn;
+
+  function init() {
+    if (isInConvo()) {
+      startChat()
+    }
+
+    startBody();
+    console.log('Messenger initiated');
+
+  }
+
+  function isInConvo() {
+    return body.classList.contains('inconversation');
+  }
+
+  function startBody() {
+    bodyObserver.observe(body, { attributes: true, attributeFilter: ['class'] });
+  }
+
+  function startChat() {
+    chatBtn = document.querySelector('.logbox div');
+    textarea = document.querySelector('.chatmsg');
+    sendBtn = document.querySelector('.sendbtn');
+    endBtn = document.querySelector('.disconnectbtn');
+
+    chatObserver.observe(chatBtn, { childList: true });
+  }
+
+  function stopChat() {
+    chatObserver.disconnect()
+  }
+
+  function stop() {
+    bodyObserver.disconnect();
+    stopChat();
+    console.log('Messenger Stopped');
+  }
+
+  function bodyCallback(recordList) {
+    recordList.forEach((record) => isInConvo() ? startChat() : stopChat());
+  }
+
+  function chatCallback(recordList) {
+    recordList.forEach((record) => {
+      record.addedNodes.forEach((node) => {
+        if (!node.className == 'logItem') return;
+        const child = node.childNodes[0];
+
+        if (child.className == 'strangermsg' || child.className == 'youmsg') {
+          msgEvent({ elem: child, class: child.className, msg: child.childNodes[2].textContent });
+        }
+      });
+    });
+  }
+
+  function send(msg) {
+    document.querySelector('.chatmsg').value = msg;
+    sendBtn.click();
+  }
+  
+
+  function endChat() {
+    if (!endBtn) throw new Error('End button isn\'t set');
+    if (!isInConvo()) return;
+    endBtn.click();
+
+    if (!isInConvo()) return;
+    endBtn.click();
+  }
+
+  return {
+    init,
+    stop,
+    send,
+    endChat,
+    msgEvent,
+  };
+}
+
+module.exports = Messenger;
